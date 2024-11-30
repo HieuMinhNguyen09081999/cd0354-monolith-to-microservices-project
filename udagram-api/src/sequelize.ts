1e9c9e1151eb64bbeddf9f134598e66a20d8fc5c
Hieu Nguyen <>
2024-11-30 12:54:41 +0700

    trigger build

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -3,11 +3,17 @@ import {config} from './config/config';
 
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
-
+  'username': config.username || "postgres",
+  'password': config.password || "mypassword",
+  'database': config.database || "postgres",
+  'host': config.host || "database-2.cdydsrnijogz.us-east-1.rds.amazonaws.com",
+  'port': 5432,
   'dialect': config.dialect,
+  'dialectOptions': {
+    'ssl': {
+      'require': true,
+      'rejectUnauthorized': false,
+    },
+  },
   'storage': ':memory:',
 });
